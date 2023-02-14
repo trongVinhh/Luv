851bf4e14120fb07f13dd38c81d116f784f013c9
Trọng Vinh <21020800@vnu.edu.vn>
2023-02-14 10:04:20 +0700

    update

--- valentine.js
+++ valentine.js
@@ -1,14 +1,17 @@
 var btn = document.querySelector('.btn')
+var audio = document.querySelector('.audio')
 function btnClicked () {
     var container = document.querySelector('.container')
     var main = document.querySelector('.main')
     // var text_scroll_container = document.querySelector('#scroll-container')
     container.classList.add("container_none")
     main.classList.add("active")
+    audio.pause(); 
     // text_scroll_container.classList("container_none")
 }
 
 var btn_heart = document.getElementById('button')
+var audio_like = document.querySelector('.audio_like')
 function clicked() {
     // var btn_like = document.querySelector('.button-like')
     // var btn_liked = document.querySelector('.liked')
@@ -18,6 +21,9 @@ function clicked() {
     // }
     
     var btn_like = document.querySelector('.button-like');
+    audio_like.play()
     btn_like.classList.toggle("liked");
 
 }
+
+
